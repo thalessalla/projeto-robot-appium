4aa49bf4c61d4632b10ae1f1bf26d3ad7b02142e
thales-salla <thales.salla@dbccompany.com.br>
2025-09-03 21:21:01 -0300

    add: readme

--- resources/libs/mongo.py
+++ resources/libs/mongo.py
@@ -1,12 +1,24 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
 from robot.api.deco import keyword
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://qax:xperience@cluster0.vf6n1dh.mongodb.net/markX?retryWrites=true&w=majority&appName=Cluster0')
 
+env_path = Path(__file__).resolve().parent.parent.parent / "apps" / "api" / ".env"
+
+
+load_dotenv(dotenv_path=env_path)
+
+mongo_uri = os.getenv("MONGO_URI")
+
+client = MongoClient(mongo_uri)
 db = client['markX']
+
 
 @keyword('Remove task from database')
 def remove_task_by_name(task_name):
     collection = db['tasks']
-    collection.delete_many({'text': task_name})
-    print('Removendo a tarefa ' + task_name)
+    result = collection.delete_many({'text': task_name})
+    print(f"Removendo a tarefa '{task_name}', documentos afetados: {result.deleted_count}")
+
